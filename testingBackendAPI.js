4f814d5c9939986149f10fddfbde4bcc258eeaac
Utkarsh13124 <utkarshp579@gmail.com>
2025-11-20 21:43:27 +0530

    Debugging..

--- testingBackendAPI.js
+++ testingBackendAPI.js
@@ -1,5 +1,12 @@
+// {
+//   "token": "c788e62f4a7a9483578023f804b794d1faf074d0",
+//   "meeting_code":"aa"
+// }
+// http://localhost:8000/api/v1/users/add_to_activity
+
+LocalHost;
 {
-  "token": "c788e62f4a7a9483578023f804b794d1faf074d0",
-  "meeting_code":"aa"
+  fullname: Utkarsh;
+  username: Utkarsh13;
+  pass: Utkarsh13;
 }
-http://localhost:8000/api/v1/users/add_to_activity
